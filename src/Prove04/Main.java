1a85f22d789f6826d49878db547704e382aae92d
cfowl <goose_7@msn.com>
2021-01-30 11:48:53 -0700

    said hello to Dana from Chris

--- src/Prove04/Main.java
+++ src/Prove04/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	String name = "Dana";
 	System.out.println("Hello, my name is " + name);
+	System.out.format("Hello %s, my name is Chris.", name);
     }
 }
